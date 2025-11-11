df92c2784037f4d67531c030d65fb59762642c9e
Kryštof Matěj <krystof.matej@me.com>
2025-11-11 08:57:11 +0100

    Fix Sendable variable

--- Sources/Rubicon/Generator/TypeGenerator.swift
+++ Sources/Rubicon/Generator/TypeGenerator.swift
@@ -5,8 +5,12 @@ protocol TypeGenerator {
 
 final class TypeGeneratorImpl: TypeGenerator {
     func makeVariableCode(from declaration: TypeDeclaration) -> String {
-        let prefix = declaration.prefix.filter { $0 == .mainActor }.map(\.rawValue)
+        let prefix = declaration.prefix.filter(isAllowed).map(\.rawValue)
         return (prefix + [declaration.name]).joined(separator: " ")
+    }
+
+    private func isAllowed(prefix: TypeDeclaration.Prefix) -> Bool {
+        [TypeDeclaration.Prefix.mainActor, .sendable].contains(prefix)
     }
 
     func makeArgumentCode(from declaration: TypeDeclaration) -> String {
